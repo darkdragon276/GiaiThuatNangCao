8f642f048b94fbbb86bce8bfd1f95e5a845678dd
Nam Chu Hai <chn5hc@bosch.com>
2023-11-05 01:03:55 +0700

    add utils parser data

--- hw4_advance_algorithm/src/Utils.java
+++ hw4_advance_algorithm/src/Utils.java
@@ -7,6 +7,7 @@ import com.google.gson.stream.JsonReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -37,9 +38,27 @@ public class Utils {
         }
     }
 
+    public static String[] preProcess(String jsonTerm) {
+        String[] wordArray = Arrays
+                // split text follow regex
+                .stream(jsonTerm.split("([A-X][,.-]\\s|[,.-]\\s|[A-Z]\\s)"))
+                // remove empty element
+                .filter(s -> !(s.isBlank() || s.isEmpty()))
+                // lower case, remove white space, remove accent
+                .map(s -> Normalizer.normalize(
+                                s.toLowerCase().trim().replaceAll("\\s+", ""),
+                                Normalizer.Form.NFKD
+                        ).replaceAll("\\p{M}", "")
+                )
+                .toArray(String[]::new);
+        System.out.println(Arrays.toString(wordArray));
+        return wordArray;
+    }
+
     public static void main(String[] args) {
         List<JsonAddressObject> readJsonData = Utils.readJSONData();
         readJsonData.forEach(object -> object.setDistrict("Binh Thuan"));
+        preProcess(readJsonData.get(0).getPred_no_corret());
         Utils.writeJSONData(readJsonData);
     }
 }
